53375dc030eafd80f28300661d0005924f98c8ec
Paweł Józiak <pjoziak@gmail.com>
2022-05-13 13:57:47 +0200

    Object learns the boundaries on-line -- adjustments in free-fall.py

--- pygame-examples/free-fall.py
+++ pygame-examples/free-fall.py
@@ -10,28 +10,15 @@ def main():
         y=100,
         height=40,
         width=40,
-        v_x=0,
-        v_y=0,
-        boundary_x=Window.HEIGHT,
-        boundary_y=Window.WIDTH,
-        color=Colors.PINK,
-    )
-    obj2 = Object(
-        x=200,
-        y=300,
-        height=40,
-        width=40,
-        v_x=25,
-        v_y=-60,
-        boundary_x=Window.HEIGHT,
-        boundary_y=Window.HEIGHT,
+        v_x=15,
+        v_y=15,
         color=Colors.PINK,
     )
     timestep = 100
     pygame.init()
     clock = pygame.time.Clock()
     surface = pygame.display.set_mode(
-        Window.SIZE
+        Window.SIZE, pygame.RESIZABLE
     )  # Displaying on specified window size
     pygame.display.set_caption("Our game")
     run = True
@@ -42,10 +29,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:  # To quit on clicking the X
                 run = False
-        obj.update(Gravity.Earth, timestep / 1000)
-        obj2.update(Gravity.Earth, timestep / 1000)
+        obj.update(Gravity.No, timestep / 1000)
         obj.draw(surface)
-        obj2.draw(surface)
         pygame.display.update()  # To update the display with newly added codes
 
     pygame.quit()
